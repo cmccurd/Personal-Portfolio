f2dc88050824a1a0b67e4662d54c83176bbb129d
cmccurd <cam.alex.mccurdy@gmail.com>
2022-10-11 23:23:53 -0400

    Particle component is working!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "./components/NavBar.jsx";
+import Header from "./components/Header.jsx";
+import Particle from "./components/Particle.jsx";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   render() {
     const { name } = this.props;
-    return <NavBar />;
+    return (
+      <>
+        <Particle style={{zIndex: 1}}/>
+        <NavBar />
+        <Header />
+      </>
+    );
   }
 }
 
