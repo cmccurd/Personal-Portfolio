54d17a79a1c3e108424a330bbe8fd9554caa5515
cmccurd <cam.alex.mccurdy@gmail.com>
2022-10-18 18:17:56 -0400

    made styling changes to MoreContent

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css";
 
-class Contact extends React.Component {
-  render() {
-    return (
+const Contact = () => {
+  return (
+    <div>
       <div></div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Contact;
