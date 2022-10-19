48b9a88791f26e93af3b78990c764a67412f90bd
cmccurd <cam.alex.mccurdy@gmail.com>
2022-10-19 15:05:49 -0400

    https option

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+var fs = require("fs");
 const path = require('path');
+const https = require("https");
 const app = express()
 const port = 3000
 
@@ -8,6 +10,16 @@ app.use(express.static('dist'))
 app.get('*', (request, response) => {
     response.sendFile(path.join(__dirname, '../dist/index.html'));
 });
-app.listen(port, () => {
-    console.log('app is listening on http://localhost:' + port);
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(3000, function () {
+    console.log(
+      "Example app listening on port 3000! Go to https://localhost:3000/"
+    );
+  });
